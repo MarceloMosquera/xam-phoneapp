f9000140dcc17af70f4b3db2d690b8e5e105702c
Marcelo <marmosquera@gmail.com>
2017-05-18 18:31:52 -0300

    Lab06A

--- PhoneApp/MainActivity.cs
+++ PhoneApp/MainActivity.cs
@@ -21,8 +21,8 @@ namespace PhoneApp
             var PhoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
             var TranslateButton = FindViewById<Button>(Resource.Id.TranslateButton);
             var CallButton = FindViewById<Button>(Resource.Id.CallButton);
-            var ResultTextView = FindViewById<TextView>(Resource.Id.ResultTextView);
             var CallHistoryButton = FindViewById<Button>(Resource.Id.CallHistoryButton);
+            var ValidateActivityButton = FindViewById<Button>(Resource.Id.ValidateActivityButton);
 
             CallButton.Enabled = false;
             var TranslatedNumber = string.Empty;
@@ -44,32 +44,13 @@ namespace PhoneApp
 
             };
 
-            CallButton.Click += async (object sender, System.EventArgs e) => {
+            CallButton.Click += (object sender, System.EventArgs e) => {
                 var CallDialog = new AlertDialog.Builder(this);
                 CallDialog.SetMessage($"Llamar al: {TranslatedNumber}");
                 CallDialog.Show();
 
                 phoneNumbers.Add(TranslatedNumber);
                 CallHistoryButton.Enabled = true;
-
-                SALLab06.ServiceClient ServiceClient = new SALLab06.ServiceClient();
-                string StudentEmail = "marmosquera@gmail.com";
-                string Pass = "brisco10";
-                string myDevice = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
-
-                SALLab06.ResultInfo result = await ServiceClient.ValidateAsync(StudentEmail, Pass, myDevice);
-
-                //AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                ResultTextView.Text = $"{result.Status}\n{result.Fullname}\n{result.Token}";
-
-                //AlertDialog alert = builder.Create();
-                //alert.SetTitle("Resultado de la verificacion");
-                //alert.SetIcon(Resource.Drawable.Icon);
-                //alert.SetMessage($"{result.Status}\n{result.Fullname}\n{result.Token}");
-                //alert.SetButton("Ok", (s, ev) => { });
-                //alert.Show();
-
-
             };
 
  
@@ -77,6 +58,11 @@ namespace PhoneApp
             {
                 var intent = new Intent(this, typeof(CallHistoryActivity));
                 intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
+            };
+            ValidateActivityButton.Click += (object sender, System.EventArgs e) =>
+            {
+                var intent = new Intent(this, typeof(ValidateActivity));
                 StartActivity(intent);
             };
         }
---
+++ PhoneApp/ValidateActivity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PhoneApp
+{
+    [Activity(Label = "ValidateActivity")]
+    public class ValidateActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.Validate);
+            SetTitle(Resource.String.ValidateActivity);
+
+            var ValidateButton = FindViewById<Button>(Resource.Id.ValidateButton);
+            var EmailText = FindViewById<TextView>(Resource.Id.EmailText);
+            var PasswordText = FindViewById<TextView>(Resource.Id.PasswordText);
+            var ResultTextView = FindViewById<TextView>(Resource.Id.ResultTextView);
+
+
+            ValidateButton.Click += async (object sender, System.EventArgs e) => {
+                SALLab06.ServiceClient ServiceClient = new SALLab06.ServiceClient();
+                string StudentEmail = EmailText.Text; //"marmosquera@gmail.com";
+                string Pass = PasswordText.Text; //"brisco10";
+                string myDevice = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+
+                SALLab06.ResultInfo result = await ServiceClient.ValidateAsync(StudentEmail, Pass, myDevice);
+
+                ResultTextView.Text = $"{result.Status}\n{result.Fullname}\n{result.Token}";
+            };
+
+        }
+    }
+}
